99dbcd7dc7a6968a7ede20ec7e155e6f3dcf9048
Julia Benachio <julia.benachio@gmail.com>
2026-02-11 14:01:00 -0300

    Fix: removendo componentes não usados

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { EstiloGlobal } from './styles'
-import Hero from './components/Hero'
-import RestaurantesList from './components/RestaurantesList'
+
 import { BrowserRouter } from 'react-router-dom'
 import Rotas from './routes'
 import Footer from './components/Footer'
